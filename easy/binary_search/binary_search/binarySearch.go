67eaea5169be76b6c3a44db1f1328e9bd0d17ce2
Alexander <kozhukhovmib@gmail.com>
2023-06-12 23:31:43 +0300

    binary search

---
+++ easy/binary_search/binary_search/binarySearch.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func search(nums []int, target int) int {
+	if len(nums) == 1 && nums[0] == target {
+		return 0
+	}
+	l, r := 0, len(nums)-1
+	for l <= r {
+		mid := (l + r) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			r = mid - 1
+		} else {
+			l = mid + 1
+		}
+	}
+	return -1
+}
+
+func main() {
+	nums := []int{-1, 0, 3, 5, 9, 12}
+	target := 9
+	fmt.Println(search(nums, target)) // must be 4
+}

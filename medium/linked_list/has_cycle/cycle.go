d78a26c79bd610592c35a85c83b77dc25828dd50
Николай Кожух <kozhukhovmib@gmail.com>
2023-06-18 22:13:50 +0300

    find node where cycle begins

---
+++ medium/linked_list/has_cycle/cycle.go
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/
+
+package main
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			slow = head
+			for slow != fast {
+				slow = slow.Next
+				fast = fast.Next
+			}
+			return slow
+		}
+	}
+	return nil
+}
